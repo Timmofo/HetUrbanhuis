98de443e8d0ae208741db815b9e91a3e5ca49ea1
Rick Lee <rick.lee0629@hotmail.com% git config --global user.email rick.lee0629@hotmail.com>
2019-11-19 18:30:11 +0800

    Removed padding of the hamburger icon to realign

--- template-parts/navbar/navbarmain.php
+++ template-parts/navbar/navbarmain.php
@@ -9,7 +9,7 @@
 
     <nav id="navbarmain" class="navbar navbar-expand-lg fixed-top">
         <div class="container">
-            <button class="navbar-toggler pt-3" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon">
                     <i class="fas fa-bars navbarmain__icon"></i>
                 </span>
